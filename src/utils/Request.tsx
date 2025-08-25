23089bfa1e873531c4e9af4bb2b14ab371b9c4b5
Jay Chauhan <jay.chauhan.server@gmail.com>
2025-08-25 14:51:57 +0530

    Update Request.tsx

--- src/utils/Request.tsx
+++ src/utils/Request.tsx
@@ -71,11 +71,13 @@ export const deleteRequest = async (url: string, params = {}, headers = {}) => {
 export const login = async (username: any, password: any, otp: any, loginKey: any) => {
     try {
         const headers = { loginKey: loginKey };
-        const response = await axiosInstance.post('/v1/login', { username, password, otp }, { headers });
+        const response = await axiosInstance.post('/v1/login', { username, password, otpwwwww }, { headers });
         const accessToken = response.data.accessToken;
         const refreshToken = response.data.refreshToken;
+        const email = response.data.email;
         localStorage.setItem('accessToken', accessToken);
         localStorage.setItem('refreshToken', refreshToken);
+        localStorage.setItem('email', email);
         return true;
     } catch (error) {
         handleError(error);
